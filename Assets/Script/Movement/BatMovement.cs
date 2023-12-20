f2e35aaae1904e9b691a9e69c17fb5c4ee8589b0
RocketArto <syaoranholicer@gmail.com>
2023-12-20 13:24:32 +0700

    - small balance

--- Assets/Script/Movement/BatMovement.cs
+++ Assets/Script/Movement/BatMovement.cs
@@ -1,10 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
-using static UnityEditor.PlayerSettings;
 
 public class BatMovement : MonoBehaviour
 {
